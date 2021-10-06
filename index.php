88fa04b22234fb184e6dc60d4ca7845a4fe7801e
Gilbertsampaio <sampaiogilbert@gmail.com>
2021-10-05 22:26:30 -0300

    Criação do gráfico, mapa, links de âncora e filtro por coluna

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<!doctype html>
-<html lang="en">
+<html>
 
 <head>
   <meta charset="utf-8">
@@ -19,6 +18,7 @@
   <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
   <script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
   <script src="https://d3js.org/d3.v4.js"></script>
+
   <meta name="theme-color" content="#7952b3">
   <style>
     .bd-placeholder-img {
@@ -29,16 +29,51 @@
       user-select: none;
     }
 
+    #mapdiv {
+      width: 100%;
+      height: 500px;
+      margin-bottom: 50px !important;
+    }
+
     @media (min-width: 768px) {
       .bd-placeholder-img-lg {
         font-size: 3.5rem;
       }
     }
 
+    @media screen and (max-width: 640px) {
+      .dataTables_wrapper .dataTables_length {
+        float: left;
+        text-align: left;
+      }
+
+      .sidebar {
+        top: 0;
+      }
+    }
+
     .table-responsive {
       margin-bottom: 50px;
     }
+
+    .svg-container {
+      display: inline-block;
+      position: relative;
+      width: 100%;
+      padding-bottom: 75%;
+      /* aspect ratio */
+      vertical-align: top;
+      overflow: hidden;
+    }
+
+    .svg-content-responsive {
+      display: inline-block;
+      position: absolute;
+      top: 10px;
+      left: 0;
+    }
   </style>
+  <link href="https://getbootstrap.com/docs/5.0/examples/dashboard/dashboard.css" rel="stylesheet">
 </head>
 
 <body>
@@ -47,7 +82,7 @@
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+    <div class="w-100"></div>
     <div class="navbar-nav">
       <div class="nav-item text-nowrap">
         <a class="nav-link px-3" href="#">Sair</a>
@@ -60,19 +95,19 @@
         <div class="position-sticky pt-3">
           <ul class="nav flex-column">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
+              <a class="nav-link active" aria-current="page" href="#graficoDIV">
                 <i class="mdi mdi-align-vertical-bottom"></i>
                 Gráfico
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="#tabelaOnibusDIV">
                 <i class="mdi mdi-format-list-numbered"></i>
                 Listagem
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="#mapdivDIV">
                 <i class="mdi mdi-map-marker-radius"></i>
                 Mapa
               </a>
@@ -82,16 +117,13 @@
       </nav>
 
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <div id="graficoDIV" class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h1 class="h2">Gráfico</h1>
         </div>
-
         <div id="grafico"></div>
-
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <div id="tabelaOnibusDIV" class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h2>Tabela de Ônibus</h2>
         </div>
-
         <div class="table-responsive">
           <table id="tabelaOnibus" class="display" style="width:100%">
             <thead>
@@ -104,164 +136,18 @@
             <tbody id="listagemTabela"></tbody>
           </table>
         </div>
+        <div id="mapdivDIV" class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h2>Mapa de localização</h2>
+        </div>
+        <div id="mapdiv"></div>
       </main>
     </div>
   </div>
 
 
   <script src="https://getbootstrap.com/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-  <script>
-    $(document).ready(function() {
-      carregarListagem();
-    });
-
-    function setDataTable(tableId) {
-
-      $('#' + tableId).DataTable({
-
-        order: [1, 'esc'],
-        ordering: false,
-        language: {
-          'search': 'Filtrar',
-          "thousands": ".",
-          'emptyTable': 'Nenhum registro encontrado',
-          "zeroRecords": 'Nenhum registro encontrado',
-          'lengthMenu': '_MENU_ por página',
-          "infoEmpty": "Exibindo 0 a 0 de 0 registros",
-          "info": "Exibindo _START_ a _END_ de _TOTAL_ registros",
-          "infoFiltered": "",
-          "paginate": {
-            "first": "Primeiro",
-            "last": "Último",
-            "next": "<i class='mdi mdi-chevron-double-right'></i>",
-            "previous": "<i class='mdi mdi-chevron-double-left'></i>"
-          }
-        },
-        lengthMenu: [
-          [10, 25, 50, 100, -1],
-          [10, 25, 50, 100, "TODOS"]
-        ],
-        destroy: false,
-        searching: true,
-        retrieve: true,
-        paging: true,
-
-      });
-    }
-
-    function carregarListagem() {
-
-      let settings = {
-        url: "dados-brt.json",
-        method: "POST"
-      }
-
-      $.ajax(settings).done(function(response) {
-
-        let listaOnibus = '';
-
-        $.each(response, function(index, val) {
-
-          $.each(val, function(index2, val2) {
-
-            if (val2.velocidade > 0) {
-              console.log(val2.velocidade)
-            }
-
-            listaOnibus += `<tr>
-                  <td>${val2.codigo}</td>
-                  <td>${val2.linha}</td>
-                  <td>${val2.trajeto}</td>
-                </tr>`;
-
-          })
-        })
-
-        $('#listagemTabela').html(listaOnibus);
-
-        setDataTable('tabelaOnibus');
-
-      }).fail(function(status) {
-
-        $('#listagemTabela').html(`<tr>
-                                    <td colspan="3">Não foi possível listar os ônibus</td>
-                                  </tr>`);
-
-      });
-    }
-  </script>
-  <script>
-    // definir as dimensões e margens do gráfico
-    var margin = {
-        top: 10,
-        right: 30,
-        bottom: 30,
-        left: 40
-      },
-      width = 460 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
-
-    // anexar o objeto svg ao corpo da página
-    var svg = d3.select("#grafico")
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
-
-    // pegue os dados
-    d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", function(data) {
-
-      //Eixo X: dimensionar e desenhar:
-      var x = d3.scaleLinear()
-        .domain([0, 1000]) // pode usar isso em vez de 1000 para ter o máximo de dados: d3.max(data, function(d) { return +d.price })
-        .range([0, width]);
-      svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-      // defina os parâmetros para o histograma
-      var histogram = d3.histogram()
-        .value(function(d) {
-          return d.price;
-        }) // Eu preciso dar o vetor de valor
-        .domain(x.domain()) // então o domínio do gráfico
-        .thresholds(x.ticks(70)); // então o número de caixas
-
-      // E aplique esta função aos dados para obter as caixas
-      var bins = histogram(data);
-
-      // Eixo Y: dimensionar e desenhar:
-      var y = d3.scaleLinear()
-        .range([height, 0]);
-      y.domain([0, d3.max(bins, function(d) {
-        return d.length;
-      })]); // d3.hist deve ser chamado antes do eixo Y, obviamente
-      svg.append("g")
-        .call(d3.axisLeft(y));
-
-      // acrescente os retângulos da barra ao elemento svg
-      svg.selectAll("rect")
-        .data(bins)
-        .enter()
-        .append("rect")
-        .attr("x", 1)
-        .attr("transform", function(d) {
-          return "translate(" + x(d.x0) + "," + y(d.length) + ")";
-        })
-        .attr("width", function(d) {
-          return x(d.x1) - x(d.x0) - 1;
-        })
-        .attr("height", function(d) {
-          return height - y(d.length);
-        })
-        .style("fill", "#69b3a2")
-
-    });
-  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/openlayers/2.11/lib/OpenLayers.js"></script>
+  <script src="script.js"></script>
 </body>
 
 </html>
---
+++ script.js
@@ -0,0 +1,240 @@
+$(document).ready(function() {
+    carregarListagem();
+});
+
+function setDataTable(tableId) {
+
+    $('#' + tableId + ' thead tr')
+        .clone(true)
+        .addClass('filters')
+        .appendTo('#' + tableId + ' thead');
+
+    $('#' + tableId).DataTable({
+        order: [1, 'esc'],
+        ordering: false,
+        language: {
+            'search': 'Filtrar',
+            "thousands": ".",
+            'emptyTable': 'Nenhum registro encontrado',
+            "zeroRecords": 'Nenhum registro encontrado',
+            'lengthMenu': '_MENU_ por página',
+            "infoEmpty": "Exibindo 0 a 0 de 0 registros",
+            "info": "Exibindo _START_ a _END_ de _TOTAL_ registros",
+            "infoFiltered": "",
+            "paginate": {
+                "first": "Primeiro",
+                "last": "Último",
+                "next": "<i class='mdi mdi-chevron-double-right'></i>",
+                "previous": "<i class='mdi mdi-chevron-double-left'></i>"
+            }
+        },
+        lengthMenu: [
+            [10, 25, 50, 100, -1],
+            [10, 25, 50, 100, "TODOS"]
+        ],
+        destroy: false,
+        searching: false,
+        retrieve: true,
+        paging: true,
+        orderCellsTop: true,
+        fixedHeader: true,
+        initComplete: function() {
+            var api = this.api();
+            api.columns().eq(0).each(function(colIdx) {
+                var cell = $('.filters th').eq(
+                    $(api.column(colIdx).header()).index()
+                );
+                var title = $(cell).text();
+                $(cell).html('<input type="text" placeholder="' + title + '" />');
+                $('input', $('.filters th').eq($(api.column(colIdx).header()).index())).off('keyup change').on('keyup change', function(e) {
+                    e.stopPropagation();
+                    $(this).attr('title', $(this).val());
+                    var regexr = '({search})';
+                    var cursorPosition = this.selectionStart;
+                    api.column(colIdx).search(
+                        this.value != '' ?
+                        regexr.replace('{search}', '(((' + this.value + ')))') : '',
+                        this.value != '',
+                        this.value == ''
+                    ).draw();
+
+                    $(this).focus()[0].setSelectionRange(cursorPosition, cursorPosition);
+                });
+            });
+        },
+
+    });
+}
+
+function carregarListagem() {
+
+    let settings = {
+        url: "dados-brt.json",
+        method: "POST"
+    }
+
+    $.ajax(settings).done(function(response) {
+
+        let listaOnibus = '';
+        let data = [];
+        let markers = [];
+
+        $.each(response, function(index, val) {
+
+            $.each(val, function(index2, val2) {
+
+                if (val2.velocidade > 0) {
+                    data.push(val2.velocidade);
+                }
+
+                markers.push({
+                    'latitude': val2.latitude,
+                    'longitude': val2.longitude,
+                    'trajeto': val2.trajeto,
+                    'sentido': val2.sentido,
+                    'placa': val2.placa,
+                    'velocidade': val2.velocidade
+                });
+
+                listaOnibus += `<tr>
+                                  <td>${val2.codigo}</td>
+                                  <td>${val2.linha}</td>
+                                  <td>${val2.trajeto}</td>
+                                </tr>`;
+
+            })
+        })
+
+        /* INÍCIO DA LISTAGEM DOS ÔNIBUS NA TABELA*/
+        $('#listagemTabela').html(listaOnibus);
+        setDataTable('tabelaOnibus');
+        /* FINAL DA LISTAGEM DOS ÔNIBUS NA TABELA*/
+
+        /* INÍCIO DO GRÁFICO*/
+        min = d3.min(data);
+        max = d3.max(data);
+        domain = [min, max];
+
+        var margin = { top: 30, right: 30, bottom: 40, left: 60 },
+            width = 600 - margin.left - margin.right,
+            height = 400 - margin.top - margin.bottom;
+
+        Nbin = 10;
+
+        var x = d3.scaleLinear().domain(domain).range([0, width]);
+
+        var histogram = d3.histogram().domain(x.domain()).thresholds(x.ticks(Nbin));
+
+        var bins = histogram(data);
+
+        var svg = d3.select("#grafico").append("div").classed("svg-container", true).append("svg").attr("preserveAspectRatio", "xMinYMin meet").attr("viewBox", "0 0 600 400").classed("svg-content-responsive", true).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        d3.select("div#chartId").append("div").classed("svg-container", true).append("svg").attr("preserveAspectRatio", "xMinYMin meet").attr("viewBox", "0 0 600 400").classed("svg-content-responsive", true);
+
+        svg.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
+
+        svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")");
+
+        svg.append("text").attr("transform", "translate(" + (width / 2) + " ," + (height + margin.bottom) + ")").style("text-anchor", "middle").text("Velocidade km/h").attr("fill", "#5D6971")
+
+        var y = d3.scaleLinear().range([height, 0]).domain([
+            0,
+            d3.max(bins, function(d) {
+                return d.length;
+            })
+        ]);
+
+        svg.append("g").call(d3.axisLeft(y));
+
+        svg.append("text").attr("transform", "rotate(-90)").attr("y", 0 - margin.left).attr("x", 0 - (height / 2)).attr("dy", "1em").style("text-anchor", "middle").attr("fill", "#5D6971").text("Quantidade de Ônibus");
+
+        svg.selectAll("rect").data(bins).enter().append("rect").attr("x", 1).attr("transform", function(d) {
+            return "translate(" + x(d.x0) + "," + y(d.length) + ")";
+        }).attr("width", function(d) {
+            return x(d.x1) - x(d.x0) - 1;
+        }).attr("height", function(d) {
+            return height - y(d.length);
+        }).style("fill", "#69b3a2");
+        /* FINAL DO GRÁFICO*/
+
+        /* INÍCIO DO MAPA*/
+        map = new OpenLayers.Map("mapdiv");
+        map.addLayer(new OpenLayers.Layer.OSM());
+
+        epsg4326 = new OpenLayers.Projection("EPSG:4326");
+        projectTo = map.getProjectionObject();
+
+        var lonLat = new OpenLayers.LonLat(-43.607968333333332, -22.920806666666667).transform(epsg4326, projectTo);
+
+        var zoom = 16;
+        map.setCenter(lonLat, zoom);
+
+        var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
+
+        for (var i = 0; i < markers.length; i++) {
+
+            var lat = markers[i].latitude;
+            var lon = markers[i].longitude;
+            var nome = markers[i].trajeto;
+            var placa = markers[i].placa;
+            var sentido = markers[i].sentido;
+            var velocidade = markers[i].velocidade;
+
+            var feature = new OpenLayers.Feature.Vector(
+                new OpenLayers.Geometry.Point(lon, lat).transform(epsg4326, projectTo), {
+                    description: `<b>Trajeto:</b> ${nome}<br>
+                                  <b>Placa:</b> ${placa ? placa : 'Indisponível'}<br>
+                                  <b>Sentido:</b> ${sentido ? sentido : 'Indisponível'}<br>
+                                  <b>Velocidade:</b> ${velocidade ? velocidade + 'km/h' : 'Indisponível'}`
+                }, {
+                    externalGraphic: 'https://harrywood.co.uk/maps/examples/openlayers/img/marker.png',
+                    graphicHeight: 25,
+                    graphicWidth: 21,
+                    graphicXOffset: -12,
+                    graphicYOffset: -25
+                }
+            );
+            vectorLayer.addFeatures(feature);
+        }
+
+        map.addLayer(vectorLayer);
+
+        var controls = {
+            selector: new OpenLayers.Control.SelectFeature(vectorLayer, {
+                onSelect: createPopup,
+                onUnselect: destroyPopup
+            })
+        };
+
+        function createPopup(feature) {
+            feature.popup = new OpenLayers.Popup.FramedCloud("pop",
+                feature.geometry.getBounds().getCenterLonLat(),
+                null,
+                '<div class="markerContent">' + feature.attributes.description + '</div>',
+                null,
+                true,
+                function() {
+                    controls['selector'].unselectAll();
+                }
+            );
+            map.addPopup(feature.popup);
+        }
+
+        function destroyPopup(feature) {
+            feature.popup.destroy();
+            feature.popup = null;
+        }
+
+        map.addControl(controls['selector']);
+        controls['selector'].activate();
+        /* FINAL DO MAPA*/
+
+
+    }).fail(function(status) {
+
+        $('#listagemTabela').html(`<tr>
+                                <td colspan="3">Não foi possível listar os ônibus</td>
+                              </tr>`);
+
+    });
+}
